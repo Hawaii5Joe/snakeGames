ba10e63d82b0bcce6fdb5895e733abb6d5cacca5
Hawaii5Joe <jojopotato323@gmail.com>
2019-02-20 21:03:09 -0800

    figured out how to render rows

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,27 @@ class App extends Component {
 
   render() {
     return (
-      <Row tilenumber={3}>
+      <>
+      <Row tilenumbers={[1,2]}>
       </Row>
+      <Row tilenumbers={[1]}></Row>
+        <Row tilenumbers={[1]}></Row>
+        <Row tilenumbers={[1]}></Row>
+
+      </>
     );
   }
 }
 
 interface RowProps{
-  tilenumber?: number;
+  tilenumbers?: Array<number>;
 }
 
 interface RowState{
   tilenumber?: number;
 }
+
+
 
 class Row extends Component <RowProps, RowState> {
 
@@ -26,9 +34,21 @@ class Row extends Component <RowProps, RowState> {
   }
 
   render() {
-    return (
-    {const : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}
-    );
+    const num = this.props.tilenumbers;
+    var nums = [0,1,2,3,4,5,6,7,8,9]
+    var numsList = nums.map(function(value) {
+      if (!num) {
+        return <Tile key={value}></Tile>
+      }
+      if (num.includes(value)) {
+        return <Tile key={value} active={true}></Tile>
+      } else {
+        return <Tile key={value}></Tile>
+      }
+    });
+    return <div className="row">
+      {numsList}
+    </div>
   }
 }
 
